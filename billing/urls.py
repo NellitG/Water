319f019287ce9c227461d583ad1facb29c4d89f2
NellitG <kamaunelson157@gmail.com>
2025-11-07 10:26:29 +0300

    create urls for billing

--- billing/urls.py
+++ billing/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ClientListCreate, MeterReadingListCreate, ClientReading
+from .views import ClientListCreate, MeterReadingListCreate, ClientPreviousReading, CalculateBill, DashboardStats
 
 urlpatterns = [
-    path('clients/', ClientListCreate.as_view(), name='client-list-create'),
-    path('readings/', MeterReadingListCreate.as_view(), name='reading-list-create'),
-    path('clients/<int:pk>/readings/', ClientReading.as_view(), name='client-readings'),
+    path("clients/", ClientListCreate.as_view(), name="clients"),
+    path("readings/", MeterReadingListCreate.as_view(), name="meter_readings"),
+    path("clients/<int:client_id>/previous-reading/", ClientPreviousReading.as_view(), name="previous_reading"),
+    path("clients/<int:client_id>/calculate-bill/", CalculateBill.as_view(), name="calculate_bill"),
+    path("dashboard-stats/", DashboardStats.as_view(), name="dashboard_stats"),
 ]
