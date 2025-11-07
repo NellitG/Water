313ac43a864acf6074fcb2e95c362ad8839c567c
NellitG <kamaunelson157@gmail.com>
2025-11-07 10:25:13 +0300

    create serializers and modify field

--- billing/serializers.py
+++ billing/serializers.py
@@ -1,46 +1,59 @@
 from rest_framework import serializers
 from .models import Client, MeterReading
+
+
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
-        fields = ['id', 'name', 'phone', 'meter_number']
+        fields = ['id', 'name', 'phone', 'meter_number', 'created_at']
+
 
 class MeterReadingSerializer(serializers.ModelSerializer):
     client_name = serializers.ReadOnlyField(source='client.name')
+
     class Meta:
         model = MeterReading
-        fields = ['id', 'client', 'client_name', 'current_reading', 'previous_reading', 'units_consumed', 'amount', 'date']
-
+        fields = [
+            'id', 'client', 'client_name', 'meter_number',
+            'current_reading', 'previous_reading',
+            'units_consumed', 'rate_per_unit', 'receipt_number',  'amount', 'date'
+        ]
         read_only_fields = ['previous_reading', 'units_consumed', 'amount']
 
-        def create(self, validated_data):
-            client = validated_data['client']
-            current_reading = validated_data['current_reading']
+    def create(self, validated_data):
+        client = validated_data['client']
+        current_reading = validated_data['current_reading']
 
-            last = MeterReading.objects.filter(client=client).order_by('-date').first()
-            prev = last.current_reading if last else 0
+        # Get the last reading for this client
+        last = MeterReading.objects.filter(client=client).order_by('-date').first()
+        prev = last.current_reading if last else 0
 
-            if current_reading < prev:
-                raise serializers.ValidationError("Current reading cannot be less than previous reading.")
-            
-            validated_data['previous_reading'] = prev
-            validated_data['units_consumed'] = current_reading - prev
-            validated_data['amount'] = validated_data['units_consumed'] * MeterReading.RATE_PER_UNIT
+        # Validation check
+        if current_reading < prev:
+            raise serializers.ValidationError("Current reading cannot be less than previous reading.")
 
-            return super().create(validated_data)
+        # Compute billing values
+        validated_data['previous_reading'] = prev
+        validated_data['units_consumed'] = current_reading - prev
+        validated_data['amount'] = validated_data['units_consumed'] * MeterReading.RATE_PER_UNIT
 
-        def update(self, instance, validated_data):
-            client = validated_data['client']
-            current_reading = validated_data['current_reading']
+        return super().create(validated_data)
 
-            last = MeterReading.objects.filter(client=client).order_by('-date').first()
-            prev = last.current_reading if last else 0
+    def update(self, instance, validated_data):
+        client = validated_data.get('client', instance.client)
+        current_reading = validated_data.get('current_reading', instance.current_reading)
 
-            if current_reading < prev:
-                raise serializers.ValidationError("Current reading cannot be less than previous reading.")
-            
-            validated_data['previous_reading'] = prev
-            validated_data['units_consumed'] = current_reading - prev
-            validated_data['amount'] = validated_data['units_consumed'] * MeterReading.RATE_PER_UNIT
+        last = MeterReading.objects.filter(client=client).order_by('-date').first()
+        prev = last.current_reading if last else 0
 
-            return super().update(instance, validated_data)
+        if current_reading < prev:
+            raise serializers.ValidationError("Current reading cannot be less than previous reading.")
+
+        validated_data['previous_reading'] = prev
+        validated_data['units_consumed'] = current_reading - prev
+        validated_data['amount'] = validated_data['units_consumed'] * MeterReading.RATE_PER_UNIT
+
+        return super().update(instance, validated_data)
+
+
+
