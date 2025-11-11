0c5a8c384fd5dde9f1663d6ad1dfef1db93a1218
NellitG <kamaunelson157@gmail.com>
2025-11-11 12:51:24 +0300

    Fix allowed CORS url

--- water_billing/settings.py
+++ water_billing/settings.py
@@ -46,6 +46,7 @@ ROOT_URLCONF = 'water_billing.urls'
 
 CORS_ALLOWED_ORIGINS = [
     
+    "https://aqua-bill-nelsons-projects-24ef2c9d.vercel.app/"
     "https://aqua-bill-mu.vercel.app/"
     
 ]
