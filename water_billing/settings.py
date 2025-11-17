549b5bce228ab45378d8231a2b176d3f6ecba197
NellitG <kamaunelson157@gmail.com>
2025-11-17 16:05:26 +0300

    remove double slash in urls

--- water_billing/settings.py
+++ water_billing/settings.py
@@ -46,7 +46,7 @@ ROOT_URLCONF = 'water_billing.urls'
 
 CORS_ALLOWED_ORIGINS = [
     
-    "https://aqua-bill-nelsons-projects-24ef2c9d.vercel.app/"
+    "https://aqua-bill-nelsons-projects-24ef2c9d.vercel.app"
     
 ]
 
