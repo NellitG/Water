97061390f1b37215f0a5d503a67d3ac181cbe2d5
NellitG <kamaunelson157@gmail.com>
2025-12-03 12:16:14 +0300

    update client receipt modal to save receipt

--- billing/urls.py
+++ billing/urls.py
@@ -31,9 +31,10 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
 
     # Receipts
-    path("receipts/", views.ReceiptListCreate.as_view(), name="receipts"),
-    path("receipts/<int:id>/", views.ReceiptRetrieveUpdateDelete.as_view(), name="receipt_detail"),
-    path("clients/<int:client_id>/receipts/save/", views.SaveReceiptForClient.as_view(), name="save_receipt"),
-    path("clients/<int:id>/receipts/", views.ReceiptsByClient.as_view(), name="client_receipts"),
+   path("receipts/", views.ReceiptListCreate.as_view(), name="receipts"),
+   path("receipts/<int:id>/", views.ReceiptRetrieveUpdateDelete.as_view(), name="receipt_detail"),
+   path("clients/<int:client_id>/receipts/save/", views.SaveReceiptForClient.as_view(), name="save_receipt"),
+   path("clients/<int:client_id>/receipts/", views.ReceiptsByClient.as_view(), name="client_receipts"),
+
 
 ]
